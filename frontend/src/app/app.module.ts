a7b6d0f5bbee4f23eff639473156e2184b239957
Elin Svensell <elinsvensell@hotmail.com>
2022-12-28 17:22:46 +0100

    Testing grid

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { TestComponent } from './test/test.component';
 import { SignUpModule } from './sign-up/feature/sign-up.module';
 import { SignInModule } from './sign-in/feature/sign-in.module';
 import { ForgotPasswordModule } from './forgot-password/feature/forgot-password.module';
+import { Test1Component } from './test1/test1.component';
+import { Test1Module } from './test1/test1.module';
 
 @NgModule({
   declarations: [
@@ -28,6 +30,7 @@ import { ForgotPasswordModule } from './forgot-password/feature/forgot-password.
     RouterModule,
     SignInModule,
     SignUpModule,
+    Test1Module
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/test1/test1.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from '../shared/ui/button/button.module';
+import { Test1Component } from './test1.component';
+
+@NgModule({
+  declarations: [Test1Component],
+  imports: [
+    CommonModule,
+    ButtonModule
+  ],
+  exports: [Test1Component]
+})
+export class Test1Module { }
